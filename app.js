82b6f89814dc61662e0f3651fc06241c8d1bb2ad
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-07-24 09:09:44 +0400

    использовать helmet для настройки заголовков HTTP

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
 
 const cors = require('./middlewares/cors');
 const routes = require('./routes/router');
@@ -15,6 +16,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors);
+app.use(helmet());
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
