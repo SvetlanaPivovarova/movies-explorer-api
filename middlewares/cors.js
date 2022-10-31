fc924e180ebfa3d0cd25bb792042eb9335a32c6c
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-10-31 10:49:46 +0400

    cors add host

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,7 +6,7 @@ const allowedCors = [
   'http://localhost:3000',
   'https://localhost:3000',
   'http://localhost:63342',
-  'http://http://localhost:5173',
+  'http://localhost:5173',
   'https://svetlanapivovarova.github.io/mithril-token-service'
 
 ];
